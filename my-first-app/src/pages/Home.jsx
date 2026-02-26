2f391f6e7449a3e8817010269710baadf2583620
Eniola <eniolaadebanjo60@gmail.com>
2026-02-26 10:21:02 +0100

    updated the service section.

--- my-first-app/src/pages/Home.jsx
+++ my-first-app/src/pages/Home.jsx
@@ -1,5 +1,10 @@
 import './Home.css'
 import aboutImg from '../assets/about-img.jpg'
+import capuccino from '../assets/capuccino.jpg'
+import croissantImg from '../assets/croissant.jpg'
+import latteImg from '../assets/latte.jpg'
+import yoghurtImg from '../assets/yoghurt.jpg'
+import greenImg from '../assets/tea.jpg'
 
 function Home(){
     return(
@@ -26,8 +31,37 @@ function Home(){
                         <p>- John Bloom, CEO</p>
                     </div>
                 </div>
-                
+            </div>    
 
+            <div className='hero-service'>
+                <h2>Our <span>Services</span></h2>
+                <p>We Offer This Services:</p>
+                <div className='service-card'>
+                    <div className='card-content'>
+                        <img src={capuccino} width={200} height={200} alt='Capuccino' />
+                        <p>Capuccino</p>
+                    </div>
+
+                    <div className='card-content'>
+                        <img src={latteImg} width={200} height={200} />
+                        <p>Latte</p>
+                    </div>
+
+                    <div className='card-content'>
+                        <img src={greenImg} width={200} height={200} />
+                        <p>Green Tea</p>
+                    </div>
+
+                    <div className='card-content'>
+                        <img src={croissantImg} width={200} height={200} alt='croissant' />
+                        <p>Croissant</p>
+                    </div>
+
+                    <div className='card-content'>
+                        <img src={yoghurtImg} width={200} height={200} />
+                        <p>Yoghurt</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
